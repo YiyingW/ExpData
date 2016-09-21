bdfa169eb493df83bdbd46dd1583565809f49b9f
Yiying Wang <wang482@email.sc.edu>
2016-09-21 15:20:13 -0400

    feat: change values to apply code on experiment 1 data

--- LinearRegressionSimulate.py
+++ LinearRegressionSimulate.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import stats
 import pylab as pl 
 
-base = 4.4
+base = 4
 r_squared = []
 Kd = []
 changes = []
@@ -10,8 +10,8 @@ changes = []
 for i in range(0,5000):
 	change = base+0.001*i
 	changes.append(change)
-	PIC_conc = np.array([20, 50, 150, 400, 500])
-	bound = [0.622, 1.276, 2.572, 4.403, change]
+	PIC_conc = np.array([10, 30, 100, 300, 400])
+	bound = [0.368, 0.908, 2.266, 3.942, change]
 	y = bound/PIC_conc
 	regression = np.polyfit(bound, y, 1)
 
@@ -36,14 +36,14 @@ min_index = Kd.index(min(Kd))
 print r_squared[min_index]
 print changes[min_index]
 
-# pl.plot(changes, Kd)
-# pl.xlabel('simulated bound concentration for 500 uM PIC')
-# pl.ylabel('Kd (uM)')
-# pl.show()
+pl.plot(changes, Kd)
+pl.xlabel('simulated bound concentration for 400 uM PIC')
+pl.ylabel('Kd (uM)')
+pl.show()
 
 
 pl.plot(changes, r_squared, 'g')
-pl.xlabel('simulated bound concentration for 500 uM PIC')
+pl.xlabel('simulated bound concentration for 400 uM PIC')
 pl.ylabel('R^2')
 pl.show()
 
